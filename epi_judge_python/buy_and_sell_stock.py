bd49f41f20f6ab8faca2569f1f3e399e57ce9fab
ishchow <ishaat@ualberta.ca>
2018-09-23 17:45:11 -0600

    buy_and_sell_stock.py: optimal solution

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,16 +4,10 @@ from test_framework import generic_test
 def buy_and_sell_stock_once(prices):
     if not prices or len(prices) <= 1:
         return 0.0
-    maxProfit = 0
-    low, high, i = prices[0], 0, 1
-    while i < len(prices):
-        if prices[i] > low and prices[i] >= high:
-            high = prices[i]
-        else:
-            maxProfit = max(maxProfit, high - low)
-            low = prices[i]
-            high = 0
-        i += 1
+    minPrice, maxProfit = float('inf'), 0
+    for p in prices:
+        maxProfit = max(maxProfit, p - minPrice)
+        minPrice = min(minPrice, p)
     return maxProfit
 
 
