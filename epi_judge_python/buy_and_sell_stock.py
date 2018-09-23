976872802a2a74c5bb65d779cde6fd36c0ab7926
ishchow <ishaat@ualberta.ca>
2018-09-23 17:43:22 -0600

    buy_and_sell_stock.py: initial solution (doesn't work)

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
-    return 0.0
+    if not prices or len(prices) <= 1:
+        return 0.0
+    maxProfit = 0
+    low, high, i = prices[0], 0, 1
+    while i < len(prices):
+        if prices[i] > low and prices[i] >= high:
+            high = prices[i]
+        else:
+            maxProfit = max(maxProfit, high - low)
+            low = prices[i]
+            high = 0
+        i += 1
+    return maxProfit
 
 
 if __name__ == '__main__':
