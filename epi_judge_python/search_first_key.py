00d5efa5e6df4ece8f2418dd332fcd439d52b47d
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-09-28 13:39:39 -0600

    search_first_key.py: Working, optimal solution (first try)

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,10 +1,19 @@
 from test_framework import generic_test
 
-
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
-
+    if not A:
+        return -1
+    low, high, lowest = 0, len(A) - 1, -1
+    while low <= high:
+        mid = low + ((high - low) // 2)
+        if A[mid] < k:
+            low = mid + 1
+        elif A[mid] == k:
+            lowest = mid
+            high = mid - 1
+        else:
+            high = mid - 1
+    return lowest
 
 if __name__ == '__main__':
     exit(
