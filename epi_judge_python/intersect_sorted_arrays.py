eda4cc1b47ccb592cdcdf66c565ffb6531199eea
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-09-29 14:04:43 -0600

    intersect_sorted_arrays.py: Initial solution (broken due to exception)

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -2,8 +2,28 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    if not A or not B:
+        return []
+    if (len(A) == 1) and (len(B) == 1):
+        return A[0] if A == B else []
+    intrsxn = []
+    if A[0] < B[0]:
+        low, high = iter(A), iter(B)
+    else:
+        low, high = iter(B), iter(A)
+    low = next(low, None)
+    high = next(high, None)
+    while low and high:
+        if low < high:
+            low = next(low, None)
+        elif low == high:
+            if (not intrsxn) or (intrsxn[-1] != low):
+                intrsxn.append(low)
+            low = next(low, None)
+            high = next(high, None)
+        else:
+            high = next(high, None)
+    return intrsxn
 
 
 if __name__ == '__main__':
