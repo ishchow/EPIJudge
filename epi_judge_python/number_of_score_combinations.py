1c4f33f633fe9fae3ee3259dd89ef67e3b2d780d
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-09-30 12:57:29 -0600

    number_of_score_combinations.py: Initial, broken solution (runs but solves the wrong problem)

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+    cache = {}
+    def combinationsFrom(score):
+        nonlocal cache
+        nonlocal individual_play_scores
+        if score < 0:
+            return 0
+        elif score == 0:
+            return 1
+
+        if score not in cache:
+            cache[score] = sum([combinationsFrom(score - ps) for ps in individual_play_scores])
+        return cache[score]
+    return combinationsFrom(final_score)
 
 
 if __name__ == '__main__':
