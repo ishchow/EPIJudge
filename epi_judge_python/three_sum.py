9f1ccd629a56ddbe41a0dcd9c0916bcdb8ff0476
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-09-30 17:50:02 -0600

    three_sum.py: Working, optimal solution (first try, yay)

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+def has_two_sum(A, target):
+    if not A:
+        return False
+    low, high = 0, len(A) - 1
+    while low <= high:
+        currSum = A[low] + A[high]
+        if currSum == target:
+            return True
+        elif currSum < target:
+            low += 1
+        else:
+            high -= 1
+    return False
 
-def has_three_sum(A, t):
-    # TODO - you fill in here.
-    return True
+def has_three_sum(A, target):
+    A.sort()
+    for val in A:
+        newTarget = target - val
+        if has_two_sum(A, newTarget):
+            return True
+    return False
 
 
 if __name__ == '__main__':
