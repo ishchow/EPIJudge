45ce0c577aea159140977f90be27c6149d348ec5
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-09-30 16:07:55 -0600

    number_of_score_combinations.py: Working, optimal solution (had to basically copy answer though)

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -2,19 +2,19 @@ from test_framework import generic_test
 
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
-    cache = {}
-    def combinationsFrom(score):
-        nonlocal cache
-        nonlocal individual_play_scores
-        if score < 0:
-            return 0
-        elif score == 0:
-            return 1
+    # One way to reach 0.
+    num_combinations_for_score = [[1] + [0] * final_score for _ in individual_play_scores]
+    for psIdx, currPs in enumerate(individual_play_scores):
+        for score in range(1, final_score + 1):
+            without_this_play = 0
+            if psIdx >= 1:
+                without_this_play = num_combinations_for_score[psIdx - 1][score]
 
-        if score not in cache:
-            cache[score] = sum([combinationsFrom(score - ps) for ps in individual_play_scores])
-        return cache[score]
-    return combinationsFrom(final_score)
+            with_this_play = 0
+            if score >= currPs:
+                with_this_play = num_combinations_for_score[psIdx][score - currPs]
+            num_combinations_for_score[psIdx][score] = without_this_play + with_this_play
+    return num_combinations_for_score[-1][-1]
 
 
 if __name__ == '__main__':
