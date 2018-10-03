f51d78dc481892668d2330befe4d7054e7597a19
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-10-02 21:41:52 -0600

    kth_largest_in_array.py: Working solution (had to look up answer though)

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -1,21 +1,37 @@
 from test_framework import generic_test
-import heapq
+import random
 
 
 # The numbering starts from one, i.e., if A = [3, 1, -1, 2]
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k, A):
-    if not A:
-        return None
-    heap = []
-    for elem in A:
-        if len(heap) < k:
-            heapq.heappush(heap, elem)
-        elif (len(heap) == k) and (elem > A[0]):
-            heapq.heappop(heap)
-            heapq.heappush(heap, elem)
-    return heap[0]
+    def partition(A, low, high, pivotIdx):
+        pivotVal = A[pivotIdx]
+        newPivotIdx = low
+        # Move pivot to the right
+        A[pivotIdx], A[high] = A[high], A[pivotIdx]
+        # Partition A such that A = [> pivot | <= pivot]
+        for i in range(low, high):
+            if A[i] > pivotVal:
+                A[i], A[newPivotIdx] = A[newPivotIdx], A[i]
+                newPivotIdx += 1
+        # Switch new pivot with old pivot
+        A[high], A[newPivotIdx] = A[newPivotIdx], A[high]
+        return newPivotIdx
+
+    low, high = 0, len(A) - 1
+    while low <= high:
+        pivotIdx = random.randint(low, high)
+        newPivotIdx = partition(A, low, high, pivotIdx)
+        if newPivotIdx == (k - 1):
+            return A[newPivotIdx]
+        elif newPivotIdx < (k - 1):
+            low = newPivotIdx + 1
+        else: # newPivotIdx > (k - 1)
+            high = newPivotIdx - 1
+
+    return None
 
 
 if __name__ == '__main__':
