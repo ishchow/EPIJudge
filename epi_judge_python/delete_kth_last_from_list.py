510b1ddf8530f0340715b95f257902c059087834
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-10-02 23:19:22 -0600

    delete_kth_last_from_list.py: Working, brute force solution

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -1,10 +1,25 @@
 from test_framework import generic_test
-
+from list_node import ListNode
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L, k):
-    # TODO - you fill in here.
-    return None
+    sz, curr = 0, L
+    while curr:
+        sz += 1
+        curr = curr.next
+    jumpSz = sz - k
+    if jumpSz == 0:
+        delNode, L = L, L.next
+        delNode.next = None
+        return L
+    dummy = ListNode(None, L)
+    prevNode, delNode = dummy, L
+    for _ in range(jumpSz):
+        prevNode, delNode = prevNode.next, delNode.next
+    prevNode.next = delNode.next
+    delNode.next = None
+    dummy.next = None
+    return L
 
 
 if __name__ == '__main__':
