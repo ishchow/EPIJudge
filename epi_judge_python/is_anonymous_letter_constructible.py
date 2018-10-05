db10382393d1db6366ee5780dda377dafe4d8af5
Ishaat Chowdhury <ishaat@ualberta.ca>
2018-10-04 20:00:02 -0600

    is_anonymous_letter_constructible.py: Optimal solution

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,22 +1,21 @@
 from test_framework import generic_test
-
+from collections import defaultdict
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
     if len(letter_text) > len(magazine_text):
         return False
-    magChToFreq = {}
-    for ch in magazine_text:
-        if ch not in magChToFreq:
-            magChToFreq[ch] = 0
-        magChToFreq[ch] += 1
-
+    letterChToFreq = defaultdict(int)
     for ch in letter_text:
-        if ch not in magChToFreq:
-            return False
-        magChToFreq[ch] -= 1
-        if magChToFreq[ch] < 0:
-            return False
-    return True
+        letterChToFreq[ch] += 1
+
+    for ch in magazine_text:
+        if ch in letterChToFreq:
+            letterChToFreq[ch] -= 1
+            if letterChToFreq[ch] == 0:
+                del letterChToFreq[ch]
+            if not letterChToFreq:
+                break
+    return not letterChToFreq
 
 
 if __name__ == '__main__':
